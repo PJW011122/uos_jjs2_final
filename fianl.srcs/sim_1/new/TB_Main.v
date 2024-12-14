2e91ac4f5c2e886a6f87a9e5beb99a93fc2a9554
Park Junwoo <junwoo5914@gmail.com>
2024-12-14 19:32:58 +0900

    set Constraint
    Constraint 파일 설정

---
+++ fianl.srcs/sim_1/new/TB_Main.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/12/14 19:26:46
+// Design Name: 
+// Module Name: TB_Main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TB_Main(
+
+    );
+endmodule
---
+++ fianl.srcs/sources_1/new/SUB_Lcd.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/12/14 19:23:56
+// Design Name: 
+// Module Name: SUB_Lcd
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SUB_Lcd(
+
+    );
+endmodule
